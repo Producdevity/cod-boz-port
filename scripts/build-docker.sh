4662070e99ccd337f7130a714ef6b04072d3f97e
Producdevity <y.gherbi.dev@gmail.com>
2026-07-11 00:40:11 +0200

    chore(docker): clean up CFLAGS and refactor build script

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 image="${CODBOZ_BUILD_IMAGE:-codboz-armhf-builder:debian-buster}"
-flags="-O2 -std=c11 -D_GNU_SOURCE -Wall -Wextra -Werror -Iinclude -Ithird_party/lzma -march=armv7-a -mfpu=neon-vfpv4 -mfloat-abi=hard"
 
 docker build -t "$image" .
 docker run --rm \
@@ -12,6 +11,9 @@ docker run --rm \
   -v "$PWD":/src \
   -w /src \
   "$image" \
-  make clean zip CC=arm-linux-gnueabihf-gcc CFLAGS="$flags"
+  make clean zip \
+    CC=arm-linux-gnueabihf-gcc \
+    CFLAGS=-O2 \
+    TARGET_CFLAGS="-march=armv7-a -mfpu=neon-vfpv4 -mfloat-abi=hard"
 
 file build/codboz_s3e_loader build/codboz_apk_extract
